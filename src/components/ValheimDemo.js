49ebaa1b6ec0cc5ddeb2e0c02392a4a87ec80b77
Dan Chrostowski <dan@danchrostowski.com>
2022-02-12 11:45:53 -0700

    add fields

--- src/components/ValheimDemo.js
+++ src/components/ValheimDemo.js
@@ -7,7 +7,11 @@ function ValheimDemo(props) {
             <b>Discord Demo</b>
           <form action="https://api.cornblaster.com/valheim" method="POST">
   
-            <input type="text" name="name" placeholder="type something here for discord" />
+            <input type="text" name="name" placeholder="Viking Name" /><br/>
+            <input type="text" name="email" placeholder="Email Address" /><br/>
+            <input type="text" name="steam_id" placeholder="Steam ID" /><br/>
+
+            
   
             <button type="submit">
   
