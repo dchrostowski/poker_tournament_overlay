75550abcafce114f781f99c21597000a521c301c
Dan Chrostowski <dan@danchrostowski.com>
2022-06-02 08:39:55 -0600

    add marign to top of corn card

--- src/components/SpinningAceOfCorn.js
+++ src/components/SpinningAceOfCorn.js
@@ -3,11 +3,11 @@ import './spinny_stuff.css'
 
 function SpinningAceOfCorn() {
     return (
-        <div className="App">
+        <div className="Corn">
         
         <p>
           </p>
-          <div class="corn-container">
+          <div class="corn-container2" style={{marginTop:70}}>
           <img src={corn} className="App-logo moving left-corn" alt="logo" />
           </div>
       </div>
